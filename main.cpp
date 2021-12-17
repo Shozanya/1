65325ca4ec382a520c4f76333bdf9fbd45bb5370
Shozanya <94974874+Shozanya@users.noreply.github.com>
2021-12-17 14:27:42 +0300

    Update main.cpp
    Появляется белое окошко!
    В следующем апдейте попробую добавить первые текстуры

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@ using namespace sf;
 
 int main()
 {
-  RenderWindow window(VideoMode(320, 480), "TETRIS";
+  RenderWindow window(VideoMode(320, 480), "TETRIS");
+  
+  while (window.isOpen())
+  {
+    Event e;
+    while (window.pollEvent(e))
+    {
+      if (e.type == Event::Closed)
+        window.close();
+    }
+    window.clear(Color::White);
+    window.display();
+  }
   
   return 0;
 }
